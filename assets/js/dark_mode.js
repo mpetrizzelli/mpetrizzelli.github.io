ceea1639356addf95d45e90faeebb1f5d8b01821
Marianyela Petrizzelli <marianyelapetrizzelli@gmail.com>
2022-06-02 18:09:12 +0200

    update

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 document.addEventListener('DOMContentLoaded', function() {
     const mode_toggle = document.getElementById("light-toggle");
 
@@ -6,3 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+<<<<<<< Updated upstream
+=======
+=======
+document.addEventListener('DOMContentLoaded', function() {
+    const mode_toggle = document.getElementById("light-toggle");
+
+    mode_toggle.addEventListener("click", function() {
+        toggleTheme(localStorage.getItem("theme"));
+    });
+});
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
